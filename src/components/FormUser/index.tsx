827ca8c615ec15e392a8f45056702d5ee40ba710
Davi <davisilvaneves3@gmail.com>
2022-03-07 00:31:50 -0300

    Form Validation

--- src/components/FormUser/index.tsx
+++ src/components/FormUser/index.tsx
@@ -1,82 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router";
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { Spinner } from '../SpinnerLoading';
 import './style.scss';
 
 
-const FormUser: React.FC = () => {
+type FormProps = {
+  user:any,
+  userSet:any,
+  userCpf:boolean
+}
 
-  const { userCpf } = useParams() as any;
-  const isNew: Boolean = !userCpf;
+const FormUser: React.FC<FormProps> = ({user, userCpf, userSet}) => {
+
+
   const navigate = useNavigate();
-
   const [isLoading, setLoading] = useState(false);
-
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    cpf: '',
-    phone: '',
-  })
 
   const [error, setError] = useState({
     type: '',
     text: ''
   })
 
+  const [name, setName] = useState(false)
+  const [cpf, setCpf] = useState(false)
+  const [email, setEmail] = useState(false)
+  const [phone, setPhone] = useState(false)
 
-  const valueInput = (e: { target: { name: any; value: any; }; } | any) => {
-    setUser({ ...user, [e.target.name]: e.target.value })
+  const isNew: Boolean = !userCpf;
+
+  const valueInput = (e:any) => {
+    userSet({ ...user, [e.target.name]: e.target.value })
     //Validações
     if (e.target.name === 'name') {
-      if (user.name.length < 3) {
+      if (user.name.length < 2) {
         setError({ type: 'name', text: 'Este campo deve conter 3 ou mais caracteres' })
+        setName(false)
         return
       }
       setError({ type: '', text: '' })
+      setName(true)
     }
     if (e.target.name === 'email') {
       if (user.email.includes("@") === false) {
         setError({ type: 'email', text: 'Digite um email válido' })
+        setEmail(false)
         return
       }
       setError({ type: '', text: '' })
+      setEmail(true)
     }
 
     if (e.target.name === 'cpf') {
       if (user.cpf.length < 10) {
         setError({ type: 'cpf', text: 'Digite um cpf válido' })
+        setCpf(false)
         return
       }
       setError({ type: '', text: '' })
+      setCpf(true)
     }
 
     if (e.target.name === 'phone') {
       if (user.phone.length < 9) {
         setError({ type: 'phone', text: 'Digite um telefone válido' })
+        setPhone(false)
         return
       }
       setError({ type: '', text: '' })
+      setPhone(true)
     }
   }
 
-  const isEnable = user.name.length && user.email.length && user.cpf.length && user.phone.length > 0
+  const isEnable = name && email && cpf && phone === true || !isNew
 
-  useEffect(() => {
-    if (window.location.href.includes('/edit-user')) {
-      const users = JSON.parse(localStorage.users)
-      const atualUser = users.filter((item: any) => item.cpf === userCpf)
-      setUser({
-        name: atualUser[0].name,
-        email: atualUser[0].email,
-        cpf: atualUser[0].cpf,
-        phone: atualUser[0].phone
-      })
-    }
-  }, []);
 
-  function criarUser(e: any) {
+
+  function criarUser(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     setLoading(true)
     setTimeout(() => {
@@ -101,7 +100,6 @@ const FormUser: React.FC = () => {
     users[index].email = user.email
     users[index].cpf = user.cpf
     users[index].phone = user.phone
-
     localStorage.removeItem('users')
     localStorage.setItem('users', JSON.stringify(users))
     navigate('/')
@@ -138,7 +136,6 @@ const FormUser: React.FC = () => {
             className={error.type === 'cpf' ? 'error' : 'style-input'}
             type='text'
             name='cpf'
-            inputMode='decimal'
             value={user.cpf}
             maxLength={11}
             onChange={valueInput}>
@@ -157,7 +154,6 @@ const FormUser: React.FC = () => {
           <button type='submit' disabled={!isEnable}>{
             isLoading? <Spinner/> : isNew ? 'Cadastrar' : 'Atualizar'
           }</button>
-
         </form>
       </div>
     </div>
--- src/pages/AddUsers/index.tsx
+++ src/pages/AddUsers/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 import FormUser from '../../components/FormUser';
 
 const AddUsers: React.FC = () => {
+
+  const { userCpf } = useParams() as any;
+  
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    cpf: '',
+    phone: '',
+  })
+
+  useEffect(() => {
+    if (window.location.href.includes('/edit-user')) {
+      const users = JSON.parse(localStorage.users)
+      const atualUser = users.filter((item: any) => item.cpf === userCpf)
+      setUser({
+        name: atualUser[0].name,
+        email: atualUser[0].email,
+        cpf: atualUser[0].cpf,
+        phone: atualUser[0].phone
+      })
+    }
+  }, []);
+
   return(
-          <FormUser/>
+          <FormUser
+          user={user}
+          userSet={setUser}
+          userCpf={userCpf}
+          />
   );
 }
 
